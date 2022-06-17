d805769efe329da19a49d137a7afadd3835dd6ad
Wim Hambrouck <wim.hambrouck@ehb.be>
2022-06-17 14:38:15 +0200

    Add project files.

---
+++ ASPDotNet_Validatie.Oef1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPDotNet_Validatie.Oef1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
